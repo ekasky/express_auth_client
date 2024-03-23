ee5ae59cbe26f67fd380606c3f7dc9c52af67071
Evan Kasky <ekasky25@gmail.com>
2024-03-22 23:43:49 -0400

    Updates mobile navbar styles 	- Fixes nested a tag in desktop navbar menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,64 +2,59 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { cn } from "@/lib/utils";
 
-
 import { Button } from "@/components/ui/button"
-
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from "@/components/ui/collapsible";
-
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+import {Sheet,SheetClose,SheetContent,SheetFooter,SheetHeader,SheetTitle,SheetTrigger,} from "@/components/ui/sheet";
+import {Collapsible,CollapsibleContent,CollapsibleTrigger,} from "@/components/ui/collapsible";
+import {NavigationMenu,NavigationMenuContent,NavigationMenuItem,NavigationMenuLink,NavigationMenuList,NavigationMenuTrigger,} from "@/components/ui/navigation-menu";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 
-import { FaBars, FaChevronDown, FaCircleNodes } from "react-icons/fa6";
+import { FaBars, FaChevronDown, FaChevronUp, FaCircleNodes, FaHtml5, FaMicrosoft, FaJava, FaPython, FaRobot, FaGamepad, FaReact, FaDesktop, FaLinux } from "react-icons/fa6";
+import { SiMongodb, SiNextdotjs } from "react-icons/si";
+import { LuBrackets } from "react-icons/lu";
+import { PiBracketsCurlyBold } from "react-icons/pi";
+import { TbBrandJavascript } from "react-icons/tb";
+import { GiMicrochip } from "react-icons/gi";
 
 import logo from "../assets/logos/bear.png"
 
+type menuComponentType = {
 
-const programmingComponents:{title: string; href: string; description: string;}[] = [
+    title: string;
+    href: string;
+    page?: React.ReactNode;
+    icon: React.ReactNode;
+    description: string;
+    
+
+};
+
+const programmingComponents:menuComponentType[] = [
 
     {
         title: "C",
         href: "/c",
+        icon: <LuBrackets size={24} />,
         description: "How to code in the C programming language"
     },
 
     {
         title: "C++",
         href: "/cpp",
+        icon: <PiBracketsCurlyBold size={24} />,
         description: "How to code in the C++ prorgramming langauge"
     },
 
     {
         title: "Java",
         href: "/java",
+        icon: <FaJava size={24} />,
         description: "How to code in the Java programming langauge"
     },
 
     {
         title: "Python",
         href: "/python",
+        icon: <FaPython size={24} />,
         description: "How to code in the Python programming langauge"
     },
 
@@ -67,76 +62,87 @@ const programmingComponents:{title: string; href: string; description: string;}[
 
         title: "JavaScript",
         href: "/javascript",
+        icon: <TbBrandJavascript size={24} />,
         description: "How to code in the JavaScript programming langauge"
 
     }
 
 ];
 
-const websiteDevelopmentComponents:{title: string; href: string; description: string;}[] = [
+const websiteDevelopmentComponents:menuComponentType[] = [
 
     {
         title: "HTML/CSS/JS",
         href: "/html-css-js",
+        icon: <FaHtml5 size={24} />,
         description: "How to make websites with HTML/CSS/JS "
     },
 
     {
         title: "MERN",
         href: "/mern",
+        icon: <SiMongodb size={24} />,
         description: "How to make full stack web apps using the mern stack"
     },
 
     {
         title: "NextJS",
         href: "/nextjs",
+        icon: <SiNextdotjs size={24} />,
         description: "How to make a full stack web app using NextJS"
     },
 
     {
         title: ".NET",
         href: "/dotnet",
+        icon: <FaMicrosoft size={24} />,
         description: "How to make a fullstack web app using MS .NET"
     },
 
 
 ];
 
-const projectComponents:{title: string; href: string; description: string;}[] = [
+const projectComponents:menuComponentType[] = [
 
     {
         title: "Embedded Systems Projects",
         href: "/embedded-systems-projects",
+        icon: <GiMicrochip size={30} />,
         description: "See a list of all my embedded systems projects"
     },
 
     {
         title: "Machine Learning",
         href: "/machine-learning-projects",
+        icon: <FaRobot size={24} />,
         description: "See a list of all my machine learning projects"
     },
 
     {
         title: "Game Development",
         href: "/game-dev-projects",
+        icon: <FaGamepad size={24} />,
         description: "See projects related to game development"
     },
 
     {
         title: "Web Apps",
         href: "/web-app-projects",
+        icon: <FaReact size={24} />,
         description: "See projects deployed to the web"
     },
 
     {
         title: "Desktop Apps",
         href: "/desktop-app-projects",
+        icon: <FaDesktop size={24} />,
         description: "See projects developed to run on desktops"
     },
 
     {
         title: "OS and Compilers",
         href: "/systems-programming",
+        icon: <FaLinux size={24} />,
         description: "See projects about Operating Systems and writing compilers"
     },
 
@@ -216,42 +222,44 @@ function MobileMenu() {
 
             <SheetHeader>
 
-                <SheetTitle>Navigation</SheetTitle>
-                
-                <SheetDescription>
-                    Browse all the projects and tutorials I have created
-                </SheetDescription>
+                <SheetTitle className="w-full flex justify-center">
+                    <img src={logo} alt="logo" width={80} />
+                </SheetTitle>
 
             </SheetHeader>
 
             <div className="flex flex-col gap-2">
+
+                <SheetClose asChild>
+                    <Link to="/" className="text-xl w-full flex justify-between items-center border-gray-300 mt-3">Home</Link>
+                </SheetClose>
+                
                 <Collapsible
-                open={isProjectsOpen}
-                onOpenChange={() => { setIsProjectsOpen(!isProjectsOpen); setIsWebDevOpen(false); setIsProgrammingOpen(false); }}
-                className="mt-4"
-                >
+                    open={isProjectsOpen}
+                    onOpenChange={() => { setIsProjectsOpen(!isProjectsOpen); setIsWebDevOpen(false); setIsProgrammingOpen(false); }}
+                    className="mt-4"
+                    >
 
-                <CollapsibleTrigger className="text-xl w-full flex justify-between items-center border-b border-gray-300">
-                    Projects 
-                    <FaChevronDown size={12} />
-                </CollapsibleTrigger>
+                    <CollapsibleTrigger className="text-xl w-full flex justify-between items-center border-gray-300">
+                        Website Development 
+                        {!isProjectsOpen ? <FaChevronDown size={12} /> : <FaChevronUp size={12} />}
+                    </CollapsibleTrigger>
 
-                <CollapsibleContent className={cn('overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up', "")}>
-                    
-                    <div className="flex flex-col gap-4 mt-4">
-                        {projectComponents.map( (component, key) =>(
+                    <CollapsibleContent className={cn('overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up', "")}>
+                        
+                        <div className="flex flex-col gap-4 mt-4 w-full">
+                            {projectComponents.map( (component, key) =>(
 
-                            <SheetClose asChild key={key}>
-                                <Link to={component.href} className="bg-gray-200 p-4 rounded-md" onClick={closeAll}>
-                                    <h3 className="font-semibold">{component.title}</h3>
-                                    <p className="text-gray-600">{component.description}</p>
-                                </Link>
-                            </SheetClose>
+                                <SheetClose asChild key={key}>
+                                    <Link to={component.href} className="border border-gray-200 shadow-md p-4 rounded-md" onClick={closeAll}>
+                                        <h3 className="font-semibold flex items-center justify-start gap-4">{component.icon}{component.title}</h3>
+                                    </Link>
+                                </SheetClose>
 
-                        ))}
-                    </div>
+                            ))}
+                        </div>
 
-                </CollapsibleContent>
+                    </CollapsibleContent>
 
                 </Collapsible>
 
@@ -261,20 +269,19 @@ function MobileMenu() {
                 className="mt-4"
                 >
 
-                <CollapsibleTrigger className="text-xl w-full flex justify-between items-center border-b border-gray-300">
+                <CollapsibleTrigger className="text-xl w-full flex justify-between items-center border-gray-300">
                     Website Development 
-                    <FaChevronDown size={12} />
+                    {!isWebDevOpen ? <FaChevronDown size={12} /> : <FaChevronUp size={12} />}
                 </CollapsibleTrigger>
 
                 <CollapsibleContent className={cn('overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up', "")}>
                     
-                    <div className="flex flex-col gap-4 mt-4">
+                    <div className="flex flex-col gap-4 mt-4 w-full">
                         {websiteDevelopmentComponents.map( (component, key) =>(
 
                             <SheetClose asChild key={key}>
-                                <Link to={component.href} className="bg-gray-200 p-4 rounded-md" onClick={closeAll}>
-                                    <h3 className="font-semibold">{component.title}</h3>
-                                    <p className="text-gray-600">{component.description}</p>
+                                <Link to={component.href} className="border border-gray-200 shadow-md p-4 rounded-md" onClick={closeAll}>
+                                    <h3 className="font-semibold flex items-center justify-start gap-4">{component.icon}{component.title}</h3>
                                 </Link>
                             </SheetClose>
 
@@ -285,27 +292,26 @@ function MobileMenu() {
 
                 </Collapsible>
 
-
+                
                 <Collapsible
                 open={isProgrammingOpen}
                 onOpenChange={() => { setIsProjectsOpen(false); setIsWebDevOpen(false); setIsProgrammingOpen(!isProgrammingOpen); }}
                 className="mt-4"
                 >
 
-                <CollapsibleTrigger className="text-xl w-full flex justify-between items-center border-b border-gray-300">
-                    Learn to Program 
-                    <FaChevronDown size={12} />
+                <CollapsibleTrigger className="text-xl w-full flex justify-between items-center border-gray-300">
+                    Programming Tutorials 
+                    {!isProgrammingOpen ? <FaChevronDown size={12} /> : <FaChevronUp size={12} />}
                 </CollapsibleTrigger>
 
                 <CollapsibleContent className={cn('overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up', "")}>
                     
-                    <div className="flex flex-col gap-4 mt-4">
+                    <div className="flex flex-col gap-4 mt-4 w-full">
                         {programmingComponents.map( (component, key) =>(
 
                             <SheetClose asChild key={key}>
-                                <Link to={component.href} className="bg-gray-200 p-4 rounded-md" onClick={closeAll}>
-                                    <h3 className="font-semibold">{component.title}</h3>
-                                    <p className="text-gray-600">{component.description}</p>
+                                <Link to={component.href} className="border border-gray-200 shadow-md p-4 rounded-md" onClick={closeAll}>
+                                    <h3 className="font-semibold flex items-center justify-start gap-4">{component.icon}{component.title}</h3>
                                 </Link>
                             </SheetClose>
 
@@ -315,9 +321,13 @@ function MobileMenu() {
                 </CollapsibleContent>
 
                 </Collapsible>
+
+                <SheetClose asChild>
+                    <Link to="/all-categories" className="text-xl w-full flex justify-between items-center border-gray-300 mt-3">All Categories</Link>
+                </SheetClose>
                 
                 <SheetClose asChild>
-                    <Link to="/about" className="text-xl w-full flex justify-between items-center border-b border-gray-300 mt-3">About</Link>
+                    <Link to="/about" className="text-xl w-full flex justify-between items-center border-gray-300 mt-3">About</Link>
                 </SheetClose>
                 
             </div>
@@ -363,6 +373,13 @@ function DesktopMenu() {
         <NavigationMenu>
 
                         <NavigationMenuList>
+
+
+                        <NavigationMenuItem>
+
+                            <Link to="/" className={navigationMenuTriggerStyle()}>Home</Link>
+
+                        </NavigationMenuItem>
 
 
                             <NavigationMenuItem>
@@ -482,14 +499,17 @@ function DesktopMenu() {
                                 </NavigationMenuContent>
 
                             </NavigationMenuItem>
+                            
+                            <NavigationMenuItem>
 
+                                <Link to="/all-categories" className={navigationMenuTriggerStyle()}>All Categories</Link>
+
+                            </NavigationMenuItem>
 
                             <NavigationMenuItem>
 
-                                <Link to="/about" className="">
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        About
-                                    </NavigationMenuLink>
+                                <Link to="/about" className={navigationMenuTriggerStyle()}>
+                                    About
                                 </Link>
 
                             </NavigationMenuItem>
