b64f2939798aa9c143293371f778d9b17adbb8d5
Evan Kasky <ekasky25@gmail.com>
2024-03-25 17:04:07 -0400

    Extracts nav items to own file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,152 +1,17 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { cn } from "@/lib/utils";
-
 import { Button } from "@/components/ui/button"
 import {Sheet,SheetClose,SheetContent,SheetFooter,SheetHeader,SheetTitle,SheetTrigger,} from "@/components/ui/sheet";
 import {Collapsible,CollapsibleContent,CollapsibleTrigger,} from "@/components/ui/collapsible";
 import {NavigationMenu,NavigationMenuContent,NavigationMenuItem,NavigationMenuLink,NavigationMenuList,NavigationMenuTrigger,} from "@/components/ui/navigation-menu";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { projectComponents, websiteDevelopmentComponents, programmingComponents } from "@/navItems";
 
-import { FaBars, FaChevronDown, FaChevronUp, FaCircleNodes, FaHtml5, FaMicrosoft, FaJava, FaPython, FaRobot, FaGamepad, FaReact, FaDesktop, FaLinux } from "react-icons/fa6";
-import { SiMongodb, SiNextdotjs } from "react-icons/si";
-import { LuBrackets } from "react-icons/lu";
-import { PiBracketsCurlyBold } from "react-icons/pi";
-import { TbBrandJavascript } from "react-icons/tb";
-import { GiMicrochip } from "react-icons/gi";
+import { FaBars, FaChevronDown, FaChevronUp, FaCircleNodes } from "react-icons/fa6";
+
 
 import logo from "../assets/logos/bear.png"
-
-type menuComponentType = {
-
-    title: string;
-    href: string;
-    page?: React.ReactNode;
-    icon: React.ReactNode;
-    description: string;
-    
-
-};
-
-const programmingComponents:menuComponentType[] = [
-
-    {
-        title: "C",
-        href: "/c",
-        icon: <LuBrackets size={24} />,
-        description: "How to code in the C programming language"
-    },
-
-    {
-        title: "C++",
-        href: "/cpp",
-        icon: <PiBracketsCurlyBold size={24} />,
-        description: "How to code in the C++ prorgramming langauge"
-    },
-
-    {
-        title: "Java",
-        href: "/java",
-        icon: <FaJava size={24} />,
-        description: "How to code in the Java programming langauge"
-    },
-
-    {
-        title: "Python",
-        href: "/python",
-        icon: <FaPython size={24} />,
-        description: "How to code in the Python programming langauge"
-    },
-
-    {
-
-        title: "JavaScript",
-        href: "/javascript",
-        icon: <TbBrandJavascript size={24} />,
-        description: "How to code in the JavaScript programming langauge"
-
-    }
-
-];
-
-const websiteDevelopmentComponents:menuComponentType[] = [
-
-    {
-        title: "HTML/CSS/JS",
-        href: "/html-css-js",
-        icon: <FaHtml5 size={24} />,
-        description: "How to make websites with HTML/CSS/JS "
-    },
-
-    {
-        title: "MERN",
-        href: "/mern",
-        icon: <SiMongodb size={24} />,
-        description: "How to make full stack web apps using the mern stack"
-    },
-
-    {
-        title: "NextJS",
-        href: "/nextjs",
-        icon: <SiNextdotjs size={24} />,
-        description: "How to make a full stack web app using NextJS"
-    },
-
-    {
-        title: ".NET",
-        href: "/dotnet",
-        icon: <FaMicrosoft size={24} />,
-        description: "How to make a fullstack web app using MS .NET"
-    },
-
-
-];
-
-const projectComponents:menuComponentType[] = [
-
-    {
-        title: "Embedded Systems Projects",
-        href: "/embedded-systems-projects",
-        icon: <GiMicrochip size={30} />,
-        description: "See a list of all my embedded systems projects"
-    },
-
-    {
-        title: "Machine Learning",
-        href: "/machine-learning-projects",
-        icon: <FaRobot size={24} />,
-        description: "See a list of all my machine learning projects"
-    },
-
-    {
-        title: "Game Development",
-        href: "/game-dev-projects",
-        icon: <FaGamepad size={24} />,
-        description: "See projects related to game development"
-    },
-
-    {
-        title: "Web Apps",
-        href: "/web-app-projects",
-        icon: <FaReact size={24} />,
-        description: "See projects deployed to the web"
-    },
-
-    {
-        title: "Desktop Apps",
-        href: "/desktop-app-projects",
-        icon: <FaDesktop size={24} />,
-        description: "See projects developed to run on desktops"
-    },
-
-    {
-        title: "OS and Compilers",
-        href: "/systems-programming",
-        icon: <FaLinux size={24} />,
-        description: "See projects about Operating Systems and writing compilers"
-    },
-
-];
 
 export default function Navbar() {
 
---
+++ src/navItems.tsx
@@ -0,0 +1,138 @@
+import { FaDesktop, FaGamepad, FaHtml5, FaJava, FaLinux, FaMicrosoft, FaPython, FaReact, FaRobot } from "react-icons/fa6";
+import { GiMicrochip } from "react-icons/gi";
+import { LuBrackets } from "react-icons/lu";
+import { PiBracketsCurlyBold } from "react-icons/pi";
+import { SiMongodb, SiNextdotjs } from "react-icons/si";
+import { TbBrandJavascript } from "react-icons/tb";
+
+type menuComponentType = {
+
+    title: string;
+    href: string;
+    page?: React.ReactNode;
+    icon: React.ReactNode;
+    description: string;
+    
+
+};
+
+
+export const programmingComponents:menuComponentType[] = [
+
+    {
+        title: "C",
+        href: "/c",
+        icon: <LuBrackets size={24} />,
+        description: "How to code in the C programming language"
+    },
+
+    {
+        title: "C++",
+        href: "/cpp",
+        icon: <PiBracketsCurlyBold size={24} />,
+        description: "How to code in the C++ prorgramming langauge"
+    },
+
+    {
+        title: "Java",
+        href: "/java",
+        icon: <FaJava size={24} />,
+        description: "How to code in the Java programming langauge"
+    },
+
+    {
+        title: "Python",
+        href: "/python",
+        icon: <FaPython size={24} />,
+        description: "How to code in the Python programming langauge"
+    },
+
+    {
+
+        title: "JavaScript",
+        href: "/javascript",
+        icon: <TbBrandJavascript size={24} />,
+        description: "How to code in the JavaScript programming langauge"
+
+    }
+
+];
+
+export const websiteDevelopmentComponents:menuComponentType[] = [
+
+    {
+        title: "HTML/CSS/JS",
+        href: "/html-css-js",
+        icon: <FaHtml5 size={24} />,
+        description: "How to make websites with HTML/CSS/JS "
+    },
+
+    {
+        title: "MERN",
+        href: "/mern",
+        icon: <SiMongodb size={24} />,
+        description: "How to make full stack web apps using the mern stack"
+    },
+
+    {
+        title: "NextJS",
+        href: "/nextjs",
+        icon: <SiNextdotjs size={24} />,
+        description: "How to make a full stack web app using NextJS"
+    },
+
+    {
+        title: ".NET",
+        href: "/dotnet",
+        icon: <FaMicrosoft size={24} />,
+        description: "How to make a fullstack web app using MS .NET"
+    },
+
+
+];
+
+export const projectComponents:menuComponentType[] = [
+
+    {
+        title: "Embedded Systems Projects",
+        href: "/embedded-systems-projects",
+        icon: <GiMicrochip size={30} />,
+        description: "See a list of all my embedded systems projects"
+    },
+
+    {
+        title: "Machine Learning",
+        href: "/machine-learning-projects",
+        icon: <FaRobot size={24} />,
+        description: "See a list of all my machine learning projects"
+    },
+
+    {
+        title: "Game Development",
+        href: "/game-dev-projects",
+        icon: <FaGamepad size={24} />,
+        description: "See projects related to game development"
+    },
+
+    {
+        title: "Web Apps",
+        href: "/web-app-projects",
+        icon: <FaReact size={24} />,
+        description: "See projects deployed to the web"
+    },
+
+    {
+        title: "Desktop Apps",
+        href: "/desktop-app-projects",
+        icon: <FaDesktop size={24} />,
+        description: "See projects developed to run on desktops"
+    },
+
+    {
+        title: "OS and Compilers",
+        href: "/systems-programming",
+        icon: <FaLinux size={24} />,
+        description: "See projects about Operating Systems and writing compilers"
+    },
+
+];
